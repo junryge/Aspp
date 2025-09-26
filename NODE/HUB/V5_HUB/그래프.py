a80d5c1cd41dabcf5f898614716c05fd8a9d167b
JUN <ggg3gkr2@gmail.com>
2025-09-26 16:56:28 +0900

    그래프.py 업데이트

--- NODE/HUB/V5_HUB/그래프.py
+++ NODE/HUB/V5_HUB/그래프.py
@@ -3,60 +3,88 @@ import plotly.graph_objects as go
 
 try:
     # 1. CSV 파일 불러오기
-    # 코드 파일과 HH8.CSV 파일을 같은 폴더에 위치시켜 주세요.
     df = pd.read_csv('HH8.CSV')
 
     # 2. 날짜 형식 변환
-    # '날짜'와 '예측날짜' 컬럼을 컴퓨터가 이해할 수 있는 날짜 형식으로 바꿉니다.
     df['날짜'] = pd.to_datetime(df['날짜'])
     df['예측날짜'] = pd.to_datetime(df['예측날짜'])
+
+    # --- ✨ 개선된 부분: 정보창에 표시할 날짜/시간 데이터 준비 ---
+    # 그래프 정보창에 'YYYY-MM-DD' 형식의 날짜와 'HH:MM:SS' 형식의 시간을 따로 표시하기 위해 새로운 컬럼을 만듭니다.
+    df['표시_날짜'] = df['날짜'].dt.strftime('%Y-%m-%d')
+    df['표시_시간'] = df['날짜'].dt.strftime('%H:%M:%S')
+    df['표시_예측날짜'] = df['예측날짜'].dt.strftime('%Y-%m-%d')
+    df['표시_예측시간'] = df['예측날짜'].dt.strftime('%H:%M:%S')
+    # ---------------------------------------------------------
 
     # 3. 그래프 객체 생성
     fig = go.Figure()
 
-    # 4. '실제값' 라인 추가
+    # 4. '실제값' 라인 추가 (✨ 정보창 형식 수정)
     fig.add_trace(go.Scatter(
         x=df['날짜'],
         y=df['실제값'],
         mode='lines',
         name='실제값 (Actual)',
-        hovertemplate='<b>날짜</b>: %{x}<br><b>실제값</b>: %{y}<extra></extra>'
+        # customdata를 이용해 새로 만든 날짜/시간 데이터를 정보창으로 전달
+        customdata=df[['표시_날짜', '표시_시간']],
+        # hovertemplate에서 customdata를 사용해 원하는 형식으로 정보를 표시
+        hovertemplate=(
+            '<b>날짜</b>: %{customdata[0]}<br>'
+            '<b>시간</b>: %{customdata[1]}<br>'
+            '<b>실제값</b>: %{y}'
+            '<extra></extra>' # 추가 정보(trace 이름) 숨기기
+        )
     ))
 
-    # 5. '예측값' 라인 추가 (점선으로 표시)
+    # 5. '예측값' 라인 추가 (✨ 정보창 형식 수정)
     fig.add_trace(go.Scatter(
         x=df['예측날짜'],
         y=df['예측값'],
         mode='lines',
         name='예측값 (Predicted)',
         line=dict(dash='dot'),
-        hovertemplate='<b>예측날짜</b>: %{x}<br><b>예측값</b>: %{y}<extra></extra>'
+        customdata=df[['표시_예측날짜', '표시_예측시간']],
+        hovertemplate=(
+            '<b>예측날짜</b>: %{customdata[0]}<br>'
+            '<b>예측시간</b>: %{customdata[1]}<br>'
+            '<b>예측값</b>: %{y}'
+            '<extra></extra>'
+        )
     ))
 
-    # 6. '점프예측' 지점 추가 (빨간 별 모양)
-    # '점프예측' 컬럼의 값이 'X'가 아닌 경우만 필터링하여 표시합니다.
-    jump_df = df[df['점프예측'] != 'X']
+    # 6. '점프예측' 지점 추가
+    jump_df = df[df['점프예측'] != 'X'].copy()
+    jump_df['표시_날짜'] = jump_df['날짜'].dt.strftime('%Y-%m-%d')
+    jump_df['표시_시간'] = jump_df['날짜'].dt.strftime('%H:%M:%S')
+    
     fig.add_trace(go.Scatter(
         x=jump_df['날짜'],
-        y=jump_df['실제값'], # 점프 지점을 실제값 위치에 표시
+        y=jump_df['실제값'],
         mode='markers',
         name='점프예측 (Jump)',
         marker=dict(color='red', size=10, symbol='star'),
-        hovertemplate='<b>점프예측!</b><br><b>날짜</b>: %{x}<br><b>값</b>: %{y}<extra></extra>'
+        customdata=jump_df[['표시_날짜', '표시_시간']],
+        hovertemplate=(
+            '<b>점프 예측!</b><br>'
+            '<b>날짜</b>: %{customdata[0]}<br>'
+            '<b>시간</b>: %{customdata[1]}<br>'
+            '<b>값</b>: %{y}'
+            '<extra></extra>'
+        )
     ))
 
     # 7. 그래프 레이아웃 설정
     fig.update_layout(
         title='실제값 vs 예측값 시계열 분석',
-        xaxis_title='날짜',
+        xaxis_title='타임라인',
         yaxis_title='값',
-        hovermode='x unified' # 마우스를 올렸을 때 x축 기준 모든 정보 표시
+        hovermode='x unified'
     )
 
     # 8. 그래프를 HTML 파일로 저장
-    # 이 코드를 실행하면 'interactive_graph.html' 파일이 생성됩니다.
-    fig.write_html("interactive_graph.html")
-    print("성공! 'interactive_graph.html' 파일을 열어 대화형 그래프를 확인하세요.")
+    fig.write_html("interactive_graph_v2.html")
+    print("성공! 'interactive_graph_v2.html' 파일을 열어 개선된 그래프를 확인하세요.")
 
 except FileNotFoundError:
     print("오류: 'HH8.CSV' 파일을 찾을 수 없습니다. 코드와 CSV 파일이 같은 폴더에 있는지 확인해주세요.")
